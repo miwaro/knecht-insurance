40d5078097f05c9f4e0c899a99268c4a23492043
Michael <mikerooze12@gmail.com>
2019-09-27 17:42:35 -0700

    deleted unused files

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const reload = require('reload')
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+//Enable CORS
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+
+app.use(express.static(__dirname + '/dist'));
+
+
+//load homepage
+app.get('/', (req, res) => {
+    res.send('index.html');
+});
+
+//load about page
+app.get('/about', (req, res) => {
+    res.send('about.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`);
+})
