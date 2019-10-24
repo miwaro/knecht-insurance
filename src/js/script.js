87b6327c3fcb07bdf35066ac260bdfaa9125eb24
Michael <mikerooze12@gmail.com>
2019-10-23 22:20:06 -0700

    Clean up css file and add media queries

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,19 @@
-document.getElementById("hamburger").addEventListener("click", toggleNavLinks);
+document.getElementById("hamburger-menu").addEventListener("click", toggleNavLinks);
 
 function toggleNavLinks() {
+    console.log('toggle');
     var links = document.getElementById("mobile-nav-links");
+    var hamburger = document.getElementById("hamburger-menu");
+    var mobileNavbar = document.querySelector(".mobile-nav-bar");
     var isHidden = links.classList.contains("hide"); 
 
     if (isHidden) {
         links.classList.remove("hide");
+        hamburger.classList.add("active");
+        mobileNavbar.classList.add("open");
     } else {
         links.classList.add("hide");
+        hamburger.classList.remove("active");
+        mobileNavbar.classList.remove("open");
     }
 }
