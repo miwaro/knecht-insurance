25b3316e1c95d6b2726b32285c4efbb75ad18886
Michael <mikerooze12@gmail.com>
2020-04-03 20:35:44 -0700

    Remove build

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+
 //Enable CORS
 const allowCrossDomain = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', 'https://damp-fjord-63452.herokuapp.com/');
     res.header('Access-Control-Allow-Credentials', "true");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -14,19 +15,15 @@ const allowCrossDomain = (req, res, next) => {
 }
 app.use(allowCrossDomain);
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/src'));
 
 //load homepage
 app.get('/', (req, res) => {
     res.send('index.html');
 });
 
-//load services page
-app.get('/services', (req, res) => {
-    res.send('services.html');
-});
 
 app.listen(port, () => {
     console.log(`Server is up on ${port}`);
-})
+});
 
